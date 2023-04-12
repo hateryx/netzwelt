987f75cc24fceb65c1454c1c71c5ff56d5424adf
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-04-12 21:52:27 +0800

    Completed the post-login authentication, routing and session management

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 import requests
-from helpers import territory_tree
+from helpers import login_required, territory_tree
 
 app = Flask(__name__)
+app.secret_key = "netzweltchallenge"
 
 
 @app.route('/')
+@login_required
 def index():
+
+    current_user = session["user_id"]
+    user_displayName = session['displayName']
+    user_role = session['roles']
+
     try:
         url = 'https://netzwelt-devtest.azurewebsites.net/Territories/All'
         response = requests.get(url)
@@ -23,11 +30,13 @@ def index():
 
     territories = territory_tree(territories_list)
 
-    return render_template("index.html", territories=territories)
+    return render_template("index.html", territories=territories, user=user_displayName)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    session.clear()
 
     if request.method == "POST":
         try:
@@ -45,7 +54,11 @@ def login():
             response = requests.post(url, json=data, headers=headers)
 
             if response.status_code == 200:
-                data = response
+                data = response.json()
+
+                session["user_id"] = data['username']
+                session["displayName"] = data["displayName"]
+                session["roles"] = data["roles"]
                 return redirect("/")
             else:
                 error_json = response.json()
--- helpers.py
+++ helpers.py
@@ -1,4 +1,15 @@
 import requests
+from flask import session, redirect
+from functools import wraps
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 def territory_tree(territories, parent=None):
